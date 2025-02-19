0f8abac3608388f96b6d01852661f008886165e0
tmahon94 <86294939+tmahon94@users.noreply.github.com>
2025-02-19 21:24:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TonysBookShop
+ TonysBookShop website
---
+++ footer.php
@@ -0,0 +1,52 @@
+<footer class="footer-custom px-6">
+      <div class="container-fluid">
+        <div class="row py-5">
+          
+          <div class="col-lg-3 col-sm-6">
+                <h5 class="pb-3">Go to:</h5>
+                <ul class="list-unstyled">
+                <li>
+                <a href="index.php" class="footer-link">Home</a>
+                </li>
+                <li>
+                <a href="shop.php" class="footer-link">Shop</a>
+                </li>
+                <li>
+                  <a href="contact.php" class="footer-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+          <div class="col-lg-5 col-sm-6">
+                <h5 class="pb-3">Need Help?</h5>
+                <ul class="list-unstyled">
+              
+                <li>
+                <a href="mailto:B00158733@mytudublin.ie" class="text-dark">
+                    B00158733@mytudublin.ie
+                </a>
+              </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 cold-md-6 col-sm-6">
+                <h5 class="pb-3">Keep in touch!</h5>
+                <p class="small">Keep up to date on all our latest offers!</p>
+                <form class="mb-3">
+                    <div class="input-group">
+                    <input type="email" class="form-control"
+                    placeholder="Email Address">
+                    <div class="input-group-text">
+                    <button type="button" class="btn bg-danger text-white
+                     text-uppercase font-weight-bold">Sign Up</button>
+                    </div>
+                 </div>
+                </form>
+          </div>
+          </div>
+          <div class="row">
+            <div class="col text-center border-top pt-2">
+              <p>&copy;Copyright 2025 - Made by Tony Mahon</p>
+              
+            </div>
+          </div>
+        </div>
+</footer>
